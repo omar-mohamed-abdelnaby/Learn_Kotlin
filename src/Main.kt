1122e3cb5f24f58c0010a06ed1b237315d71a26c
omar-mohamed-abdelnaby <omarmohamed112005@gmail.com>
2025-03-11 00:30:01 +0200

    update

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,27 @@
 fun main() {
-    println("omar")
-    println("mohamed")
-    print("Hello World")
+    //"val" can’t be reassigned a different value after initialization
+    val firstName2 : String ="Ali"          //Equal  -> val firstName ="Ali"
+    println(firstName2)
+
+    val myAge : Int = 21                      //Equal -> val myAge =21
+    println(myAge)
+
+
+
+    println("=========================")
+
+    //"var" can be reassigned a different value after initialization
+    var firstName  ="Omar"
+    firstName = "ami"
+    println(firstName)
+    println(firstName.isEmpty())
+
+    var lastName ="Mohamed"
+    println(lastName)
+
+    var gender  = ""
+    println(gender.isEmpty())
+
+    var age =20
+    print(age)
     }
