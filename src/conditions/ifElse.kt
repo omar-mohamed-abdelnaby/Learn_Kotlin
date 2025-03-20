f4a88fdb8c6a1f40e4ba97daac4ac038ce56e191
omar-mohamed-abdelnaby <omarmohamed112005@gmail.com>
2025-03-20 19:21:27 +0200

    update

---
+++ src/conditions/ifElse.kt
@@ -0,0 +1,23 @@
+package conditions
+
+fun main(){
+    var price = 16
+    if (price == 25){
+        println("Price is $price")
+    }
+    else if (price < 25){
+        println("Price is Good")
+    }
+    else if (price > 25){
+        println("Price is Expensive")
+    }
+    else{
+        println("Not Found")
+    }
+
+    println("==================")
+    //Single Line IfElse Statement
+
+    var age = 20
+    if (age > 20) println("Your age is $age") else println("Your age is more than $age")
+}
